7816f25fc5d8d835c07895244cd8b4bc115deb89
jayhyonhan <jayhyonhan@gmail.com>
2023-10-07 07:55:55 +0100

    also did multi processing

--- pi2.py
+++ pi2.py
@@ -1,8 +1,11 @@
 import decimal, threading, sys
-from multiprocessing import Pool
 
 fact_out = [0, 0, 0]
-def factorial_function(l):
+n=int(input("n: "))
+
+sys.setrecursionlimit(2147483647)
+
+def fact(l):
     n=l[0]
     i=l[1]
     factorial = 1
@@ -19,30 +22,18 @@ def func1(precision):
     return 426880 * d
 
 def func2(k):
-    #threading
-    #_a = threading.Thread(target=factorial_function, args=[[6*k, 0]])
-    #_b = threading.Thread(target=factorial_function, args=[[3*k, 1]])
-    #_c = threading.Thread(target=factorial_function, args=[[k, 2]])
-    #_a.start()
-    #_b.start()
-    #_c.start()
-    #print("_a:%s\n_b:%s\n_c:%s"%(_a.name, _b.name, _c.name))
-    #_c.join()
-    #_b.join()
-    #_a.join()
-    #parallel proccesing
-    
-    # only for multiproccesing
-    _pool = Pool()
-    _a = _pool.apply_async(func=factorial_function, args=([6*k, 0]))
-    _b = _pool.apply_async(func=factorial_function, args=([3*k, 1]))
-    _c = _pool.apply_async(func=factorial_function, args=([k, 2]))
-    _c.wait()
-    _b.wait()
-    _a.wait()
+    _a = threading.Thread(target=fact, args=[[6*k, 0]])
+    _b = threading.Thread(target=fact, args=[[3*k, 1]])
+    _c = threading.Thread(target=fact, args=[[k, 2]])
+    _a.start()
+    _b.start()
+    _c.start()
+    print("_a:%s\n_b:%s\n_c:%s"%(_a.name, _b.name, _c.name))
+    _c.join()
+    _b.join()
+    _a.join()
     a = decimal.Decimal(fact_out[0]*(545140134*k+13591409))
     b = decimal.Decimal(fact_out[1]*(fact_out[2]**3)*((-262537412640768000)**k))
-    print(a,b)
     res = a / b
     if k > 0:
         return res + func2(k - 1)
@@ -51,11 +42,9 @@ def func2(k):
 
 def pi_chudnovsky(k, precision):
       return func1(precision)/func2(k)
-if __name__ == '__main__':
-    n=int(input("n: "))
-    print("start")
-    sys.setrecursionlimit(2147483647)
-    p = 10*n+1
-    decimal.getcontext().prec = p
-    pi = pi_chudnovsky(n*2, p)
-    print(str(pi)[n+1])
+
+p = 10*n+1
+decimal.getcontext().prec = p
+pi = pi_chudnovsky(n*2, p)
+
+print(str(pi)[n+1])
---
+++ pi2_multiproc.py
@@ -0,0 +1,56 @@
+import decimal, sys
+from multiprocessing import Process, Manager
+
+def main():
+    global fact_out
+    manager = Manager()
+    fact_out = manager.list([0, 0, 0])
+    n=int(input("n: "))
+    sys.setrecursionlimit(2147483647)
+    sys.set_int_max_str_digits(2147483647)
+    p = 10*n+1
+    decimal.getcontext().prec = p
+    pi = pi_chudnovsky(n*2, p)
+    print(str(pi)[n+1])
+
+def fact(l):
+    n=l[0]
+    _i=l[1]
+    fact_out = l[2]
+    factorial = 1
+    while n>0:
+          factorial*=n
+          n-=1
+    fact_out[_i] = factorial
+
+def func1(precision):
+    p = decimal.getcontext().prec
+    decimal.getcontext().prec = precision
+    d = decimal.Decimal(10005).sqrt()
+    decimal.getcontext().prec = p
+    return 426880 * d
+
+def func2(k):
+    arguments = [[6*k, 0, fact_out], [3*k, 1, fact_out], [k, 2, fact_out]]
+    procs = []
+    for argument in arguments:
+        proc = Process(target=fact, args=(argument,))
+        procs.append(proc)
+        proc.start()
+        print(proc.name)
+
+    for proc in procs:
+        proc.join()
+    a = decimal.Decimal(fact_out[0]*(545140134*k+13591409))
+    b = decimal.Decimal(fact_out[1]*(fact_out[2]**3)*((-262537412640768000)**k))
+    res = a / b
+    if k > 0:
+        return res + func2(k - 1)
+    else:
+        return res
+
+def pi_chudnovsky(k, precision):
+      return func1(precision)/func2(k)
+
+if __name__ == '__main__':
+    main()
